b4b01f0c669ab1f0ebaf2b4459e8664698266cc2
Orji <ndubuisiaso@gmail.com>
2022-11-26 15:16:33 +0100

    first commit

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const { MongoClient } = require('mongodb')
 
 let dbConnection
 
+let uri = 'mongodb+srv://mongo-tutorial:mongo-tutorial@cluster0.qyn8s5e.mongodb.net/?retryWrites=true&w=majority'
+
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://0.0.0.0:27017/bookstore')
+    MongoClient.connect(uri)
       .then(client => {
         dbConnection = client.db()
         return cb()
